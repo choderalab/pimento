2dffdc301c9d5d8d540815695bfe912f3354f26c
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-09-12 20:50:20 -0400

    Updates to TR01

--- kab/TR01/code/plot_tics.py
+++ kab/TR01/code/plot_tics.py
@@ -1,3 +1,4 @@
+import os
 import mixtape.featurizer, mixtape.tica, mixtape.cluster, mixtape.markovstatemodel, mixtape.ghmm
 import numpy as np
 import mdtraj as md
@@ -5,62 +6,29 @@ from parameters import load_trajectories, build_full_featurizer
 import sklearn.pipeline, sklearn.externals.joblib
 import mixtape.utils
 
-system = "NSD2"
+system = os.path.split(os.getcwd())[1]
 out_filename = "/home/kyleb/src/choderalab/pimento/kab/TR01/tex/figures/%s_tics.png" % system
 
 trj0, trajectories, filenames = load_trajectories()
 
+pipeline = sklearn.externals.joblib.load("./pipeline.job")
+feature_pipeline = sklearn.externals.joblib.load("./feature_pipeline.job")
+cluster_pipeline = sklearn.externals.joblib.load("./cluster_pipeline.job")
 
 X_all = feature_pipeline.transform(trajectories)
 q = np.concatenate(X_all)
 
+cluster = cluster_pipeline.steps[-1][1]
 covars_ = cluster.covars_
 covars_ = cluster.covars_.diagonal(axis1=1, axis2=2)
-
-for i, j in [(0, 1)]:
-    figure()
-    title("%d" % n_states)
-    hexbin(q[:,i], q[:, j], bins='log')
-    errorbar(cluster.means_[:, i], cluster.means_[:, j], xerr=covars_[:,i] ** 0.5, yerr=covars_[:, j] ** 0.5, fmt='kx', linewidth=4)
-
-offset = np.ones(2) * 0.05
-for state in range(n_states):    
-    plt.annotate("%d" % state, cluster.means_[state, 0:2] + offset, fontsize='x-large')
-
-
-
-pipeline = sklearn.externals.joblib.load("./partial_pipeline.job")
-full_pipeline = sklearn.externals.joblib.load("./full_pipeline.job")
-
-X_all = pipeline.fit_transform(trajectories)
-q = np.concatenate(X_all)
-
-model = full_pipeline.steps[-1][1]
 
 title("Slow Coordinates in %s" % system)
 i, j = 0, 1
 hexbin(q[:,i], q[:, j], bins='log')
+#errorbar(cluster.means_[:, i], cluster.means_[:, j], xerr=covars_[:,i] ** 0.5, yerr=covars_[:, j] ** 0.5, fmt='kx', linewidth=4)
+#offset = np.ones(2) * 0.05
+#for state in range(n_states):    
+#    plt.annotate("%d" % state, cluster.means_[state, 0:2] + offset, fontsize='x-large')
 ylabel("2nd Slowest Coordinate")
 xlabel("1st Slowest Coordinate")
 savefig(out_filename, bbox_inches="tight")
-
-
-
-
-#figure()
-#i, j = 0, 1
-#hexbin(q[:,i], q[:, j], bins='log')
-
-#offset = np.ones(2) * 0.05
-#errorbar(model.means_[states, i], model.means_[states, j], xerr=model.vars_[states,i] ** 0.5, yerr=model.vars_[states, j] ** 0.5, fmt='kx', linewidth=4)
-
-
-"""
-i, j = 0, 1
-hexbin(q[:,i], q[:, j], bins='log')
-errorbar(model.means_[:, i], model.means_[:, j], xerr=model.vars_[:,i] ** 0.5, yerr=model.vars_[:, j] ** 0.5, fmt='kx', linewidth=4)
-
-offset = np.ones(2) * 0.05
-for state in range(model.n_states):
-    plt.annotate("%d" % state, model.means_[state, 0:2] + offset, fontsize='x-large')
-"""
