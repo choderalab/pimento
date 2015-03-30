b74890f41ba2f233aef75aa5c2f4a305b42a7932
kyleabeauchamp <kyleabeauchamp@gmail.com>
2015-03-30 13:56:08 -0400

    Added code and slides

---
+++ kab/march_2015_setd8/code/analyze_tica_dihedral.py
@@ -0,0 +1,39 @@
+import glob
+import mdtraj as md
+from msmbuilder import example_datasets, cluster, msm, featurizer, lumping, utils, decomposition
+from sklearn.pipeline import make_pipeline
+import itertools
+
+STRIDE = 4  # 1 ns
+MIN_LENGTH = 400 * 4
+PATH = "/home/kyleb/dat/fah_data/10478/"
+
+RUN = 1
+pdb_code = {1:"4ij8", 0:"1zkk"}[RUN]
+
+filenames = [filename for filename in glob.glob(PATH + "run%d-*.h5" % RUN) if len(md.open(filename)) > MIN_LENGTH]
+run0 = np.array(map(lambda x: "run1" in x, filenames))
+len(filenames)
+
+
+%time trajectories0 = [md.load(filename, stride=STRIDE) for filename in filenames]
+
+t_1zkk = md.load("/home/kyleb/src/choderalab/fah-projects/projects/setd8-p10478/pdbs/1ZKK_fixed1.pdb")
+t_4ij8 = md.load("/home/kyleb/src/choderalab/fah-projects/projects/setd8-p10478/pdbs/4IJ8_fixed1.pdb")
+
+atom_indices = t_1zkk.top.select("resSeq >=15 and resSeq <= 145")
+# select ends, not resi 16:144
+trajectories = [t.atom_slice(atom_indices) for t in trajectories0]  # For RMSD clustering
+
+n_states = 40
+
+dihed = featurizer.DihedralFeaturizer(types=["phi", "psi", "chi1"])
+tica = decomposition.tICA()
+clusterer = cluster.MiniBatchKMeans(n_clusters=n_states)
+microstate_model = msm.MarkovStateModel()
+pipeline = make_pipeline(dihed, tica, clusterer, microstate_model)
+
+pipeline.fit(trajectories0)
+
+X = pipeline.transform(trajectories0)
+Xf = np.concatenate(X)
---
+++ kab/march_2015_setd8/slides/slides.md
@@ -0,0 +1,65 @@
+% title: Preliminary SETD8 Analysis
+% author: Kyle A. Beauchamp
+% favicon: figures/membrane.png
+
+---
+title: Simulation
+
+- 275 trajectories > 300 ns
+- 125 $\mu$ s, average 450 ns per traj
+- Two structures: 4ij8, 1zkk
+
+---
+title: N and C termini are unstructured
+
+<img width=875 src=figures/termini_hairball.png />
+
+---
+title: N and C termini are unstructured
+
+<img width=875 src=figures/termini_hairball_cut.png />
+
+
+---
+title: N and C termini are unstructured
+
+<img width=875 src=figures/termini_hairball_cut_transparent.png />
+
+
+---
+title: N and C termini are unstructured
+
+<img width=875 src=figures/with_new_structure.png />
+
+---
+title: The two structures are disconnected
+
+<img width=480 src=figures/rmsd_from_4ij8.png />  <img width=480 src=figures/rmsd_from_1zkk.png />
+
+
+---
+title: Alternative terminal conformation?
+
+<img width=900 src=figures/c_terminus_alignment.png />
+
+
+---
+title: Takeaways
+
+- Need more than 100 $\mu s$ sampling
+- PDB structures correspond to long-lived states?
+- N and C termini rearrangements are expected
+- Treat each structure separately for now
+
+
+---
+title: Analysis: 4IJ8 Trajectories
+
+- Most of the protein remains well-structured
+
+
+---
+title: Future Work?
+
+- Simulations with SAM?
+- 
